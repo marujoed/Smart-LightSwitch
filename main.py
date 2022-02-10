0541a4bd40659087ce1791687bccffd357b02313
marujoed <>
2022-02-10 22:27:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# == Functions (Funções) ==
+  # DRY - Don't repeat yourself
+  # Realizam uma tarefa
+  # Calcula e retorna um valor
+
+
+is_day = False
+lights_on = not is_day
+
+print('Daytime?')
+print(is_day)
+
+print('Lights on?')
+print(lights_on)
